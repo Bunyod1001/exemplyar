1b3b6cc348d716adcf6977377c645c7fca864949
Bunyodbek <nematullayevbunyod1029@gamil.com>
2023-04-27 20:46:46 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Form } from './components/Form';
 import { Table } from './components/Table';
-import moduleStyle from "./App.module.css";
 import "./App.css";
 
 let Data = [
